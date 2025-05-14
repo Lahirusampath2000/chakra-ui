5314acc6ba31039eb860526cf38519abd62edfd9
Lahiru sampath <lahiru.20210981@iit.ac.lk>
2025-05-14 16:57:51 +0530

    get task title from db.json

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,30 +1,32 @@
 import { Box, SimpleGrid, Text} from "@chakra-ui/react";
+import { use } from "react";
+import { useLoaderData } from "react-router-dom";
 
 export default function Dashboard() {
+
+  const tasks = useLoaderData()
 
   
   return (
 
-    <SimpleGrid columns={4} p={10} spacing={10} minChildWidth={"250px"}>
-      <Box bg={"white"} h="200px" border="1px solid" >
-        <Text color={{base: "pink", md:"green", lg:"blue.900"}}>hello</Text>
-      </Box>
-      <Box bg={"white"} h="200px" border="1px solid" ></Box>
-      <Box bg={"white"} h="200px" border="1px solid" ></Box>
-      <Box bg={"white"} h="200px" border="1px solid" ></Box>
+    <SimpleGrid  spacing={10} minChildWidth={"300px"}>
 
-      <Box bg={"white"} h="200px" border="1px solid" ></Box>
-      <Box bg={"white"} h="200px" border="1px solid" ></Box>
-      <Box bg={"white"} h="200px" border="1px solid" ></Box>
-      <Box bg={"white"} h="200px" border="1px solid" ></Box>
-
-      <Box bg={"white"} h="200px" border="1px solid" ></Box>
-      <Box bg={"white"} h="200px" border="1px solid" ></Box>
-      <Box bg={"white"} h="200px" border="1px solid" ></Box>
-      <Box bg={"white"} h="200px" border="1px solid" ></Box>
+      {tasks && tasks.map((task) => (
+        <div key={task.id}>{task.title}</div>
+      ))}
+      
 
     </SimpleGrid>
    
     
   )
+}
+
+
+export const tasksLoader = async () => {
+  const res = await fetch("http://localhost:3000/tasks")
+  if(!res.ok) {
+    throw Error("Could not fetch the data")
+  }
+  return res.json()
 }
